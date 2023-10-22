52a38d100ac81fd2f3190fad213ff5a057442d91
Hanna Kulai <hkulai@inspirebrands.com>
2023-10-22 17:23:49 +0300

    refactor: task 1

--- src/module_4/task-1/free_space.sh
+++ src/module_4/task-1/free_space.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 DEFAULT_THRESHOLD=50
-
 THRESHOLD="${1:-${DEFAULT_THRESHOLD}}"
 
-if ! [[ "$THRESHOLD" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
-  echo "Inputs must be a numbers"
+if ! [[ "$THRESHOLD" =~ ^[0-9]{1,2}$ ]]; then
+  echo "Inputs must be a numbers and less 100%"
   exit 0
 fi
 
-echo "Observing free disk space for threshold: '$THRESHOLD' %"
+echo "Monitoring free disk space for threshold (default: ${DEFAULT_THRESHOLD}%): $THRESHOLD%"
 echo "Press CTRL+C if you want to stop..."
 
-df -h | grep -vE '^Filesystem|tmpfs|cdrom|map' | while true; do
+df -Ph | grep -vE '^Filesystem|tmpfs|cdrom|map' | awk '{ print $5,$1 }' | while true; do
   read -r data
-  current_used=$(echo "$data" | awk '{print $5}' | sed s/%//g)
-  disk=$(echo "$data" | awk '{print $1}')
+  current_used=$(echo $data | awk '{print $1}' | sed s/%//g)
 
-  if [ "$current_used" ] && [ "$current_used" -ge "$THRESHOLD" ]; then
-    echo -e "\033[31;41mWARNING: The disk \"$disk\" has used $current_used% of total available space \033[0m"
+  if [ "$data" ] && [ $current_used -ge $THRESHOLD ]; then
+    partition=$(echo $data | awk '{print $2}')
+    echo -e "\033[31;41mWARNING: The partition \"$partition\" has used $current_used% of available space \033[0m"
   fi
   sleep 2
 done
